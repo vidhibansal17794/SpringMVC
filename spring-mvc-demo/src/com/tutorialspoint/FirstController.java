b8d545e38fe2850446c6f2985b275cdd05eed3f2
vidhibansal17794 <56512561+vidhibansal17794@users.noreply.github.com>
2020-03-29 15:11:01 +0530

    Add files via upload
    Second Project of MVC with jar files

---
+++ spring-mvc-demo/src/com/tutorialspoint/FirstController.java
@@ -0,0 +1,18 @@
+package com.tutorialspoint;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FirstController {
+
+	@RequestBody
+	@RequestMapping("/")
+	
+	public String methodFirst()
+	{
+		return "HI";
+		
+	}
+}
